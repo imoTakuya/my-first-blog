640654007960d0ffadea3c2b5b4006c0c8298082
imo <a5816087@aoyama.jp>
2018-06-20 15:49:04 +0900

    My Djsngo app, first commit

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -1,46 +1,28 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.axes_rgb import RGBAxes
+import matplotlib.cbook as cbook
+import matplotlib.image as image
 
 
-def get_demo_image():
-    # prepare image
-    delta = 0.5
+# Fixing random state for reproducibility
+np.random.seed(19680801)
 
-    extent = (-3, 4, -4, 3)
-    x = np.arange(-3.0, 4.001, delta)
-    y = np.arange(-4.0, 3.001, delta)
-    X, Y = np.meshgrid(x, y)
-    Z1 = np.exp(-X**2 - Y**2)
-    Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
-    Z = (Z1 - Z2) * 2
-
-    return Z, extent
+datafile = cbook.get_sample_data('logo2.png', asfileobj=False)
+print('loading %s' % datafile)
+im = image.imread(datafile)
+im[:, :, -1] = 0.5  # set the alpha channel
 
 
-def get_rgb():
-    Z, extent = get_demo_image()
+matplotlib.rcParams['axes.unicode_minus'] = False
+fig, ax = plt.subplots()
+ax.plot(np.random.rand(20), 'o', ms=0, lw=0, alpha=0.7, mfc='orange')
+ax.plot(10*np.random.randn(100), 10*np.random.randn(100), 'o')
 
-    Z[Z < 0] = 0.
-    Z = Z / Z.max()
-
-    R = Z[:13, :13]
-    G = Z[2:, 2:]
-    B = Z[:13, 2:]
-
-    return R, G, B
+ax.grid()
+fig.figimage(im, 10, 10, zorder=3)
 
 
-fig = plt.figure(1)
-ax = RGBAxes(fig, [0.1, 0.1, 0.8, 0.8])
+ax.set_title('Using hyphen instead of Unicode minus')
 
-r, g, b = get_rgb()
-kwargs = dict(origin="lower", interpolation="nearest")
-ax.imshow_rgb(r, g, b, **kwargs)
-
-ax.RGB.set_xlim(0., 9.5)
-ax.RGB.set_ylim(0.9, 10.6)
-
-
-plt.draw()
 plt.show()
